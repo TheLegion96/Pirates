25999fbc55af79db09ff553b6c05de6fde3923c4
Andrea tonnini <at.rsm@hotmail.it>
2018-09-04 20:57:11 +0200

    Starter pack Level Editor

---
+++ Assets/Scripts/TonnoScript/StarterPackScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[HideMonoScript]
+public class TestLevelEditor : MonoBehaviour {
+
+   
+	// Use this for initialization
+	void Start () {
+      
+            transform.DetachChildren();
+        DestroyImmediate(this.gameObject);
+	}
+	
+
+}
